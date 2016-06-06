b3a7a0d4537261cc0f8ed3681440a38474a0cbf6
JP <JP@JP>
2016-06-06 09:09:21 +0200

    Renombramos la clase Prin por Main para asi diferenciar cual es l clase principal y creamos un metodo llamado print que realizara la accion de imprimir el mensaje  mostrando el resultado obtenido

---
+++ src/examenfinalparte1/Main.java
@@ -0,0 +1,14 @@
+package examenfinalparte1;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+         new Principal(10, 20);
+         new Principal(10);
+         new Principal();
+      
+      
+    }
+}
--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,31 @@
 package examenfinalparte1;
+
 public class Principal {
+
     int i;
-public Principal(int j, int m) {i=met(j,m);}
+
+    public Principal(int j, int m) {
+        i = suma(j, m);
+        print();
+    }
+
     public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+        i = suma(j, j);
+        print();
+        
+    }
+
+    public Principal() {
+        i = -1;
+        print();
+    }
+
+    public int suma(int x, int x1) {
+        return x + x1;
+    }
+    
+    public void print(){
+        System.out.println("Es:"+this.i);
+    
+    }
+}
